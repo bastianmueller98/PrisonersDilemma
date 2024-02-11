7160f3302bfe7d3238a0ccc9317b641e3e10e23f
bastianmueller98 <116962517+bastianmueller98@users.noreply.github.com>
2024-02-11 13:20:19 +0100

    Add player1 and player2 attributes to Game class; Add plots

--- Game.py
+++ Game.py
@@ -1,10 +1,13 @@
 from GameState import GameState
 from random import choice
+import matplotlib.pyplot as plt
 
 
 class Game:
     def __init__(
         self,
+        player1,
+        player2,
         player1_move=None,
         player2_move=None,
         payoffs={
@@ -14,6 +17,8 @@ class Game:
             ("D", "D"): (1, 1),
         },
     ) -> None:
+        self.player1 = player1
+        self.player2 = player2
         self.payoffs = payoffs
         self.history = []
         if player1_move is None or player2_move is None:
@@ -21,6 +26,18 @@ class Game:
         self.play(player1_move, player2_move)
         self.player1_total_payoff = 0
         self.player2_total_payoff = 0
+        self.player1_payoff_matrix_names = {
+            ("C", "C"): "R",
+            ("C", "D"): "S",
+            ("D", "C"): "T",
+            ("D", "D"): "P",
+        }
+        self.player2_payoff_matrix_names = {
+            ("C", "C"): "R",
+            ("C", "D"): "T",
+            ("D", "C"): "S",
+            ("D", "D"): "P",
+        }
 
     @classmethod
     def random_first_moves(cls):
@@ -32,16 +49,70 @@ class Game:
 
     def play(self, move1, move2):
         gs = GameState(
-            len(self.history) + 1, (move1, move2), self.payoffs[(move1, move2)]
+            len(self.history) + 1,
+            (move1, move2),
+            self.payoffs[(move1, move2)],
         )
         self.history.append(gs)
-        self.player1_total_payoff = sum([x.payoffs[0] for x in self.history])
-        self.player2_total_payoff = sum([x.payoffs[1] for x in self.history])
+        gs.player1_total_payoff = sum([x.payoffs[0] for x in self.history]) / len(
+            self.history
+        )
+        gs.player2_total_payoff = sum([x.payoffs[1] for x in self.history]) / len(
+            self.history
+        )
+        self.history[-1] = gs
         return gs
 
-    def end_game(self):
+    def end_game(self, plot_results=False, average_lines=False):
+        if plot_results:
+            player1_payoffs = [x.player1_total_payoff for x in self.history]
+            player2_payoffs = [x.player2_total_payoff for x in self.history]
+            rounds = list(range(1, len(self.history) + 1))
+
+            plt.plot(
+                rounds,
+                player1_payoffs,
+                label=f"Player 1: {self.player1.__class__.__name__}",
+                color="blue",
+            )
+            plt.plot(
+                rounds,
+                player2_payoffs,
+                label=f"Player 2: {self.player2.__class__.__name__}",
+                color="orange",
+            )
+            if average_lines:
+                player1_cum_mov_avg_payoff = [
+                    sum(player1_payoffs[:i]) / i
+                    for i in range(1, len(self.history) + 1)
+                ]
+                plt.plot(
+                    rounds,
+                    player1_cum_mov_avg_payoff,
+                    label="Cumulative moving average P1",
+                    color="green",
+                    linestyle="--",
+                )
+                player2_cum_mov_avg_payoff = [
+                    sum(player2_payoffs[:i]) / i
+                    for i in range(1, len(self.history) + 1)
+                ]
+                plt.plot(
+                    rounds,
+                    player2_cum_mov_avg_payoff,
+                    label="Cumulative moving average P2",
+                    color="red",
+                    linestyle="--",
+                )
+
+            plt.title("Prisoners Dilemma average payoffs")
+            plt.xlabel("Rounds")
+            plt.ylabel("Avg. Payoff")
+            plt.legend()
+            plt.show()
+
         return f"""End result after {len(self.history)} rounds:
-Player 1 total payoff: {self.player1_total_payoff}
-Player 1 avg. payoff: {self.player1_total_payoff/len(self.history)}
-Player 2 total payoff: {self.player2_total_payoff}
-Player 2 avg. payoff: {self.player2_total_payoff/len(self.history)}."""
+    Player 1 total payoff: {self.player1_total_payoff}
+    Player 1 avg. payoff: {self.player1_total_payoff/len(self.history)}
+    Player 2 total payoff: {self.player2_total_payoff}
+    Player 2 avg. payoff: {self.player2_total_payoff/len(self.history)}."""
