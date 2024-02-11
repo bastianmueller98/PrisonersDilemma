73edfa8f5a52ec7806d893475b307384d2be0a91
bastianmueller98 <116962517+bastianmueller98@users.noreply.github.com>
2024-02-11 13:21:32 +0100

    Add total payoff tracking to GameState and update player strategies in main.py

--- GameState.py
+++ GameState.py
@@ -2,5 +2,7 @@ class GameState:
     def __init__(self, round, moves, payoffs) -> None:
         self.player1_move = moves[0]
         self.player2_move = moves[1]
+        self.player1_total_payoff = None
+        self.player2_total_payoff = None
         self.round = round
         self.payoffs = payoffs
--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 from Game import Game
+from strategies.ProbabilityCooperator import ProbabilityCooperator
+from strategies.TFTT import TFTTStrategy
 from strategies.TitForTatStrategy import TitForTatStrategy
 from strategies.AlwaysDefectStrategy import AlwaysDefectStrategy
+from strategies.nPavlov import nPavlov
 
 
 def main():
-    # Initialize game
-    game = Game()
-    # print(game.history[-1].payoffs)
-
     # Initialize players/strategies
-    player1 = TitForTatStrategy(1)
-    player2 = AlwaysDefectStrategy(2)
+    player1 = nPavlov(1)
+    player2 = ProbabilityCooperator(2)
+
+    # Initialize game
+    game = Game(player1, player2, "C", "C")
 
     # Play game
-    rounds = 15
+    rounds = 10000
     for i in range(rounds):
         move1 = player1.play(game)
         move2 = player2.play(game)
         game.play(move1, move2)
-        # print(game.history[-1].payoffs)
 
     # End game and print results
-    print(game.end_game())
+    print(game.end_game(plot_results=True))
 
 
 if __name__ == "__main__":
